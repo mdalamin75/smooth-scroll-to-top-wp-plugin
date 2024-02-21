e811cc4c75dce02707638df4547ff852b607b2d6
MD.AL-AMIN <mdalmain67378@gmail.com>
2024-02-22 02:15:20 +0600

    create smooth scroll to top wp plugin

---
+++ smooth-scroll-to-top-wp.php
@@ -0,0 +1,16 @@
+<?php
+    /*
+    * Plugin Name:          Smooth Scroll To Top WP
+    * Plugin URI:           https://wordpress.org/plugins/smooth-scroll-to-top-wp/
+    * Description:          Smooth Scroll to top plugin will help you to enable back to top your WordPress website smoothly.
+    * Version:              1.0.0
+    * Requires al least:    5.2
+    * Requires PHP:         7.2
+    * Author:               MD.AL-AMIN
+    * Author URI:           https://mdalamin.vercel.app/
+    * License:              GPLv2 or later
+    * License URI:          https://www.gnu.org/licenses/gpl-2.0.html
+    * Update URI:           https://github.com/mdalamin75
+    * Text Domain:          ssttw
+    */ 
+?>
