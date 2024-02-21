d1975a050280cd58ac55e071e4b0aa2597d50529
MD.AL-AMIN <mdalmain67378@gmail.com>
2024-02-22 04:14:27 +0600

    Enqueue CSS & JS file

--- smooth-scroll-to-top-wp.php
+++ smooth-scroll-to-top-wp.php
@@ -10,7 +10,32 @@
     * Author URI:           https://mdalamin.vercel.app/
     * License:              GPLv2 or later
     * License URI:          https://www.gnu.org/licenses/gpl-2.0.html
-    * Update URI:           https://github.com/mdalamin75
+    * Update URI:           https://github.com/mdalamin75/smooth-scroll-to-top-wp-plugin
     * Text Domain:          ssttw
     */ 
+
+    // Including CSS
+    function ssttw_enqueue_style(){
+        wp_enqueue_style('ssttw-style', plugins_url('css/ssttw-style.css', __FILE__));
+    }
+    add_action('wp_enqueue_scripts', 'ssttw_enqueue_style');
+
+    // Including JavaScript
+    function ssttw_enqueue_scripts(){
+        wp_enqueue_script('jquery');
+        wp_enqueue_script('ssttw-plugin-script', plugins_url('js/ssttw-plugin.js', __FILE__), array(), '1.0.0', 'true');
+    }
+    add_action('wp_enqueue_scripts', 'ssttw_enqueue_scripts');
+
+    // jQuery Plugin Setting Activation
+    function ssttw_scroll_script(){
+        ?>
+            <script>
+                jQuery(document).ready(function () {
+                    jQuery.scrollUp();
+                })
+            </script>
+        <?php
+    }
+    add_action('wp_footer', 'ssttw_scroll_script');
 ?>
